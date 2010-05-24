nbp/Nixpkgs

commit 84b521c23b57108fc63731de7723885d68a92b08

releaseTools.sourceTarball: Make the bootstrap build inputs native.
git-svn-id: https://svn.nixos.org/repos/nix/nixpkgs/trunk@21955 70bd8c7a-acb8-0310-9f0d-9cc1c95dcdbb

pkgs/build-support/release/source-tarball.nix
@@ -4,6 +4,7 @@
 
 { officialRelease ? false
 , buildInputs ? []
+, buildNativeInputs ? []
 , name ? "source-tarball"
 , version ? "0"
 , src, stdenv, autoconf, automake, libtool
@@ -77,8 +78,8 @@ stdenv.mkDerivation (
   {
     name = name + "-" + version + versionSuffix;
 
-    buildInputs = buildInputs ++ bootstrapBuildInputs;
-    
+    buildNativeInputs = bootstrapBuildInputs ++ buildNativeInputs;
+
     postHook = ''
       ensureDir $out/nix-support
     '';  

commit 11e1c4f1441b794c8e3394c3cf6cedb66b5d0e35

releaseTools.sourceTarball: Fix regression introduced in r21955.
git-svn-id: https://svn.nixos.org/repos/nix/nixpkgs/trunk@21959 70bd8c7a-acb8-0310-9f0d-9cc1c95dcdbb

pkgs/build-support/release/source-tarball.nix
@@ -78,7 +78,7 @@ stdenv.mkDerivation (
   {
     name = name + "-" + version + versionSuffix;
 
-    buildNativeInputs = bootstrapBuildInputs ++ buildNativeInputs;
+    buildNativeInputs = buildNativeInputs ++ bootstrapBuildInputs;
 
     postHook = ''
       ensureDir $out/nix-support